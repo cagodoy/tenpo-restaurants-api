2905e5b9e845e02f2e2f52440cc8bd3f92b714ee
Camilo <cagodoy@acid.cl>
2019-12-17 06:36:34 -0300

    refactor in main.go file

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 
 	restaurantsSvc "github.com/cagodoy/tenpo-restaurants-api/rpc/restaurants"
 	"google.golang.org/grpc"
@@ -12,6 +13,8 @@ import (
 
 	pb "github.com/cagodoy/tenpo-challenge/lib/proto"
 	_ "github.com/lib/pq"
+
+	nats "github.com/nats-io/nats.go"
 )
 
 func main() {
@@ -21,8 +24,38 @@ func main() {
 		log.Println("missing env variable PORT, using default value...")
 	}
 
+	natsHost := os.Getenv("NATS_HOST")
+	if natsHost == "" {
+		natsHost = "nats"
+		log.Println("missing env variable NATS_HOST, using default value...")
+	}
+
+	natsPort := os.Getenv("NATS_PORT")
+	if natsPort == "" {
+		natsPort = "4222"
+		log.Println("missing env variable NATS_PORT, using default value...")
+	}
+
 	srv := grpc.NewServer()
-	svc := restaurantsSvc.New()
+
+	nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
+		log.Printf("Got disconnected! Reason: %q\n", err)
+	})
+	nats.ReconnectHandler(func(nc *nats.Conn) {
+		log.Printf("Got reconnected to %v!\n", nc.ConnectedUrl())
+	})
+	nats.ClosedHandler(func(nc *nats.Conn) {
+		log.Printf("Connection closed. Reason: %q\n", nc.LastError())
+	})
+	nc, err := nats.Connect("nats://"+natsHost+":"+natsPort, nats.MaxReconnects(15), nats.ReconnectWait(3*time.Second))
+	if err != nil {
+		log.Fatalf("Failed to connect to NATS servdr: %v", err)
+	}
+	log.Println("Connected to NATS server", "nats://"+natsHost+":"+natsPort)
+
+	c, _ := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
+
+	svc := restaurantsSvc.New(c)
 
 	pb.RegisterRestaurantServiceServer(srv, svc)
 	reflection.Register(srv)
