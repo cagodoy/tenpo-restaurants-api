83ebfa80b2faf290d6ee034160c987e0f4234996
Camilo <cagodoy@acid.cl>
2019-12-18 20:05:54 -0300

    refactor in restaurants.go file

--- restaurants.go
+++ restaurants.go
@@ -1,14 +1,10 @@
 package restaurants
 
 import (
+	"strconv"
+
 	pb "github.com/cagodoy/tenpo-challenge/lib/proto"
 )
-
-// Coord ...
-type Coord struct {
-	Latitude  string `json:"latitude"`
-	Longitude string `json:"longitude"`
-}
 
 // Restaurant ...
 type Restaurant struct {
@@ -23,14 +19,14 @@ type Restaurant struct {
 
 // Service ...
 type Service interface {
-	ListByCoord(coord Coord, pageToken string) ([]*Restaurant, string, error)
+	ListByCoord(coord Coord, userID string) ([]*Restaurant, error)
 }
 
 // ToProto ...
 func (r *Restaurant) ToProto() *pb.Restaurant {
 	c := &pb.Coord{
 		Latitude:  r.Coord.Latitude,
-		Longitude: r.Coord.Latitude,
+		Longitude: r.Coord.Longitude,
 	}
 
 	return &pb.Restaurant{
@@ -60,4 +56,28 @@ func (r *Restaurant) FromProto(rr *pb.Restaurant) *Restaurant {
 	r.Coord = c
 
 	return r
+}
+
+// Coord ...
+type Coord struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+// GetLatLngStr ...
+func (c *Coord) GetLatLngStr() string {
+	lat := strconv.FormatFloat(c.Latitude, 'f', -1, 64)
+	lng := strconv.FormatFloat(c.Longitude, 'f', -1, 64)
+
+	return lat + "," + lng
+}
+
+// GetLatStr ...
+func (c *Coord) GetLatStr() string {
+	return strconv.FormatFloat(c.Longitude, 'f', -1, 64)
+}
+
+// GetLngStr ...
+func (c *Coord) GetLngStr() string {
+	return strconv.FormatFloat(c.Longitude, 'f', -1, 64)
 }
